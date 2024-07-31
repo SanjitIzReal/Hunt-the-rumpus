88a1c495b8286059a57df1752b15c5702f7d3713
SanjitIzReal <159495159+SanjitIzReal@users.noreply.github.com>
2024-07-31 04:34:12 +0000

    created cave

---
+++ cave.py
@@ -0,0 +1,5 @@
+class Cave:
+    def __init__(Self, cave_name):
+        self.name = cave_name
+        self.description = none
+        
---
+++ main.py
@@ -0,0 +1,5 @@
+from cave import Cave
+
+cavern = Cave("cavern")
+
+print(cavern)
