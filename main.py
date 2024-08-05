e082fa2aa40fd791d3fb8a51d0b9179511d33578
SanjitIzReal <159495159+SanjitIzReal@users.noreply.github.com>
2024-08-05 04:49:32 +0000

    54ergthy

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 from cave import Cave
 
-cavern = Cave("cavern")
+cavern = Cave("Cavern")
 grotto = Cave("Grotto")
-dungeon = Cave("dungeon")
+dungeon = Cave("Dungeon")
+
+cavern.set_description("A damp and dirty cave")
+grotto.set_description("A small cave with ancient markings")
+dungeon.set_description("A large cave with a rack")
+
+cavern.link_cave("south", dungeon)
+dungeon.link_cave("north",cavern)
+dungeon.link_cave("west", grotto)
+grotto.link_cave("east", dungeon)
+
+dungeon.get_details()
